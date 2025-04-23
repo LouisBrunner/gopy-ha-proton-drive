11c1d9571c4b80fbf34b5e22224b124d7f54cfd3
Louis Brunner <louis.brunner.fr@gmail.com>
2025-04-23 16:32:44 +0100

    fix: resolve platform tags

--- release.sh
+++ release.sh
@@ -6,9 +6,17 @@ source venv/bin/activate
 
 GOOS=darwin GOARCH=arm64 python3 -m build
 GOOS=darwin GOARCH=amd64 python3 -m build
-GOOS=linux GOARCH=amd64 GOOS_EXTRA="many" python3 -m build
-GOOS=linux GOARCH=arm64 GOOS_EXTRA="many" python3 -m build
-GOOS=linux GOARCH=amd64 GOOS_EXTRA="musl" python3 -m build
-GOOS=linux GOARCH=arm64 GOOS_EXTRA="musl" python3 -m build
+GOOS=linux GOARCH=amd64 python3 -m build
+GOOS=linux GOARCH=arm64 python3 -m build
+GOOS=linux GOARCH=amd64 GOOS_LINUX="musllinux_1_1" python3 -m build
+GOOS=linux GOARCH=arm64 GOOS_LINUX="musllinux_1_1" python3 -m build
 
-# twine upload dist/*
+echo "Release to PyPI?"
+select yn in "yes" "no"; do
+  case $yn in
+    yes) break;;
+    no) exit;;
+  esac
+done
+
+twine upload dist/*
--- setup.py
+++ setup.py
@@ -11,16 +11,17 @@ class MyBDistWheel(bdist_wheel):
         if "GOOS" in os.environ and "GOARCH" in os.environ:
             plat = os.environ["GOOS"]
             arch = os.environ["GOARCH"]
-            plat_extra = os.getenv("GOOS_EXTRA", "")
             if arch == "amd64":
                 arch = "x86_64"
-            if arch == "arm64":
+            if arch == "arm64" and plat != "darwin":
                 arch = "aarch64"
             if arch == "386":
                 arch = "i386"
             if plat == "darwin":
-                plat = "macos_11_0"
-            self.plat_name = f"{plat_extra}{plat}_{arch}"
+                plat = "macosx_11_0"
+            if plat == "linux":
+                plat = os.environ.get("GOOS_LINUX", "manylinux2014")
+            self.plat_name = f"{plat}_{arch}"
 
         super().finalize_options()
 
